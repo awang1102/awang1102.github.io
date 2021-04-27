5ad69a1c188da748431f956cf2199cbe2bd98f4e
Alex Wang <wang.alex@berkeley.edu>
2021-04-27 01:57:51 -0700

    Add website

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('about-jump').onclick = function() {
+    Jump('#about')
+};
+
+document.getElementById('experience-jump').onclick = function() {
+    Jump('#experience')
+};
+
+document.getElementById('contact-jump').onclick = function() {
+    Jump('#contact')
+};
